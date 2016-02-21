4191a08fbb6b0015f7b331dcb887a52f7051cf4c
Vincent Montalieu <vincent.montalieu@outlook.com>
2016-02-21 13:04:46 +0100

    connexion avec le serveur EN LOCALHOST

--- app/scripts/services/constants.js
+++ app/scripts/services/constants.js
@@ -4,7 +4,7 @@
 
 angular.module('pfeWebClientApp')
   .constant('CONSTANTS', {
-    serverAddress: 'http://10.212.100.199:4000/',
-    //serverAddress: 'http://127.0.0.1:4000/',
+  	// LOCALHOST SERVER
+    serverAddress: 'http://127.0.0.1:4000/',
     sendSurveyPath: 'survey/send'
   });
